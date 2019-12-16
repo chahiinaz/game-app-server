d4ed65f690476b86afc72e630df62b39983f8494
chahiinaz <chahiinaz@gmail.com>
2019-12-16 18:38:04 +0100

    fine tuned signup post

--- signup/router.js
+++ signup/router.js
@@ -4,6 +4,7 @@ const User = require("./model");
 const bcrypt = require("bcrypt");
 
 router.post("/", (req, res, next) => {
+  console.log("body", req.body);
   const userToCreate = {
     name: req.body.name,
     password: bcrypt.hashSync(req.body.password, 10)
