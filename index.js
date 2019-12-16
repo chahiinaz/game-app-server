be4fcbad981c76d8f9ff68c04bac759358394b5c
chahiinaz <chahiinaz@gmail.com>
2019-12-16 13:33:11 +0100

    deploy to heroku

--- index.js
+++ index.js
@@ -11,8 +11,5 @@ app.use(cors());
 app.use(jsonParser);
 app.use(loginRouter);
 app.use(signUpRouter);
-app.get("/", (req, res) => {
-  res.send("hello");
-});
 
 app.listen(port, console.log(`listening to port ${port}`));
