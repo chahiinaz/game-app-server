093ed86e41b5f7c884b265c3652127d7a0bf1c49
chahiinaz <chahiinaz@gmail.com>
2019-12-17 11:21:26 +0100

    initialized gameroom stream

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const signUpRouter = require("./signup/router");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const loginRouter = require("./login/router");
+const Sse = require("json-sse");
+const stream = new Sse();
+const gameroomFactory = require("./gameroom/router");
+const gameroomRouter = gameroomFactory(stream);
 
 app.use(cors());
 app.use(jsonParser);
 app.use(loginRouter);
 app.use(signUpRouter);
+app.use(gameroomRouter);
+
+app.get("/test", (req, res) => {
+  stream.send("test");
+  res.send("hello");
+});
+
+app.get("/stream", (req, res) => {
+  stream.init(req, res);
+});
 
 app.listen(port, console.log(`listening to port ${port}`));
