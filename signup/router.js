dc41e2a7578e7b5a498614696270d7992af6313d
chahiinaz <chahiinaz@gmail.com>
2019-12-17 12:22:26 +0100

    added jwt to signuprouter

--- signup/router.js
+++ signup/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = new Router();
 const User = require("./model");
 const bcrypt = require("bcrypt");
+const { toJWT, toData } = require("../login/jwt");
 
 router.post("/signup", (req, res, next) => {
   console.log("body", req.body);
@@ -12,7 +13,8 @@ router.post("/signup", (req, res, next) => {
 
   User.create(userToCreate)
     .then(user => {
-      res.json(user);
+      const jwt = toJWT({ userId: user.id });
+      res.json({ jwt });
     })
     .catch(error => next(error));
 });
