0fa377d971591162ef7af2e4bc50867b7eabe443
chahiinaz <chahiinaz@gmail.com>
2019-12-17 13:47:21 +0100

    set up stream for all gamerooms

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Sse = require("json-sse");
 const stream = new Sse();
 const gameroomFactory = require("./gameroom/router");
 const gameroomRouter = gameroomFactory(stream);
+const Gameroom = require("./gameroom/model");
 
 app.use(cors());
 app.use(jsonParser);
@@ -22,8 +23,19 @@ app.get("/test", (req, res) => {
   res.send("hello");
 });
 
-app.get("/stream", (req, res) => {
-  stream.init(req, res);
+app.get("/stream", async (req, res, next) => {
+  try {
+    const gamerooms = await Gameroom.findAll();
+    const action = {
+      type: "ALL_GAMEROOMS",
+      payload: gamerooms
+    };
+    const string = JSON.stringify(action);
+    stream.updateInit(string);
+    stream.init(req, res);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.listen(port, console.log(`listening to port ${port}`));
