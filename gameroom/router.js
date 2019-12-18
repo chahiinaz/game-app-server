838b41c83710ffe0a0d39e9c538563cf331f948d
chahiinaz <chahiinaz@gmail.com>
2019-12-18 11:32:32 +0100

    setup endpoint gameroom.id and made action

--- gameroom/router.js
+++ gameroom/router.js
@@ -18,6 +18,20 @@ function factory(stream) {
       next(error);
     }
   });
+  router.get("/gameroom/:id", async (req, res, next) => {
+    try {
+      const gameroom = await Gameroom.findByPk(req.params.id);
+      const action = {
+        type: "ONE_GAMEROOM",
+        payload: gameroom
+      };
+      const string = JSON.stringify(action);
+      stream.send(string);
+      res.send(gameroom);
+    } catch (error) {
+      next(error);
+    }
+  });
   return router;
 }
 
