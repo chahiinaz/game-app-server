1fb0a8771361d602923bc05b576e8818c578804c
mariuspatrick <marius.patrick72@gmail.com>
2019-12-18 11:29:19 +0100

    Finish back end setup for adding users to gamerooms

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
-const port = process.env.PORT || 4000;
-const signUpRouter = require("./signup/router");
 const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
-const loginRouter = require("./login/router");
+const cors = require("cors");
 const Sse = require("json-sse");
-const stream = new Sse();
-const gameroomFactory = require("./gameroom/router");
-const gameroomRouter = gameroomFactory(stream);
+
+const signUpRouter = require("./signup/router");
+const loginRouter = require("./login/router");
 const Gameroom = require("./gameroom/model");
 
-app.use(cors());
+const User = require("./user/model");
+const gameroomFactory = require("./gameroom/router");
+const userFactory = require("./user/router");
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+const port = process.env.PORT || 4000;
+
 app.use(jsonParser);
-app.use(loginRouter);
-app.use(signUpRouter);
+app.use(cors());
+
+const stream = new Sse();
+
+const gameroomRouter = gameroomFactory(stream);
 app.use(gameroomRouter);
 
-app.get("/test", (req, res) => {
-  stream.send("test");
-  res.send("hello");
-});
+app.use(loginRouter);
+app.use(signUpRouter);
+
+const userRouter = userFactory(stream);
+app.use(userRouter);
+
+// app.get("/test", (req, res) => {
+//   stream.send("test");
+//   res.send("hello");
+// });
 
 app.get("/stream", async (req, res, next) => {
   try {
-    const gamerooms = await Gameroom.findAll();
+    const gamerooms = await Gameroom.findAll({ include: [User] });
+
     const action = {
       type: "ALL_GAMEROOMS",
       payload: gamerooms
     };
     const string = JSON.stringify(action);
+
     stream.updateInit(string);
     stream.init(req, res);
   } catch (error) {
