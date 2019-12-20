a3413186f20dace9a5ee6d0491c8c0f83fc90fa5
chahiinaz <chahiinaz@gmail.com>
2019-12-20 13:53:25 +0100

    added every constant

--- choice/router.js
+++ choice/router.js
@@ -13,24 +13,34 @@ function factory(stream) {
       const gameroom = await GameRoom.findByPk(user.gameroomId, {
         include: [User]
       });
+      const every = gameroom.users.every(user => user.choice !== null);
       const usersInGame = gameroom.users;
       const player1 = usersInGame[0];
       const player2 = usersInGame[1];
+      console.log("player1", player1);
+      console.log("player2", player2);
 
       if (player1.choice === null || player2.choice === null) return;
 
-      if (player1.choice === player2.choice && player1.choice) {
-        player1.points += 1;
-        player2.points += 1;
-      } else if (player1.choice === player2.choice && !player1.choice) {
-        player1.points += 2;
-        player2.points += 2;
-      } else if (player1.choice && !player2.choice) {
-        player1.points += 3;
-      } else if (player2.choice && !player1.choice) {
-        player2.points += 3;
+      if (every) {
+        if (player1.choice === player2.choice && player1.choice) {
+          console.log("both cooperate test");
+          player1.points += 1;
+          player2.points += 1;
+        } else if (player1.choice === player2.choice && !player1.choice) {
+          console.log("both defect test");
+          player1.points += 2;
+          player2.points += 2;
+        } else if (player1.choice && !player2.choice) {
+          console.log("1 cooperates 2 defects");
+          player1.points += 3;
+        } else if (!player1.choice && player2.choice) {
+          console.log("1 defects 2 cooperates");
+          player2.points += 3;
+        }
       }
 
+      console.log("player1points", player1.points);
       await User.update(
         { points: player1.points },
         {
@@ -39,6 +49,7 @@ function factory(stream) {
           }
         }
       );
+      console.log("player2points", player2.points);
 
       await User.update(
         { points: player2.points },
