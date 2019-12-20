752f93def5311e645649b228fec7cfadc2f6187f
chahiinaz <chahiinaz@gmail.com>
2019-12-20 09:56:05 +0100

    merging master

--- login/router.js
+++ login/router.js
@@ -29,7 +29,8 @@ router.post("/login", (req, res) => {
           // 3. if the password is correct, return a JWT with the userId of the user (user.id)
           res.send({
             jwt: toJWT({ userId: user.id }),
-            name: user.name
+            name: user.name,
+            id: user.id
           });
         } else {
           res.status(400).send({
